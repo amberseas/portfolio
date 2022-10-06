4bd2ee159811ef4c144083b92dad3dcf6e3f6996
Gintare <gintareba@gmail.com>
2022-10-06 17:33:36 +0300

    feat: add header

---
+++ my-page/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EntryComponent } from './entry/entry.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EntryComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
